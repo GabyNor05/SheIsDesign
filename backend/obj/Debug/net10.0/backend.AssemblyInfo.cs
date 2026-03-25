e4058b561f89587e27439aa4b9390588502e4cec
KBOlifant <241299@virtualwindow.co.za>
2026-03-25 14:40:12 +0200

    working login
    Bux fixes between backend and DB, user wasnt being recognized

--- backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21bc848467d4c9cdbf92227c782fd48e4f7424d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c023812993deb6a4dcfe3ddae08ba5e955c39461")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
