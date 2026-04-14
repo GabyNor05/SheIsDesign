b842f577f8da9fa4df76cd31608032d90d87592d
KBOlifant <241299@virtualwindow.co.za>
2026-04-14 20:57:00 +0200

    Dotnet update
    Ef migrations update

--- backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2967324ec7739c5f734974421986a5b1a87d42b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c24fdfb4eb68324645fc97ad432fef3f903f9c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
