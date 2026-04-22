07c25e5fce8ce1b19f570541b0958d1f8f931df6
KeaganCB-OW <160649882+KeaganCB-OW@users.noreply.github.com>
2026-04-22 18:51:38 +0200

    Build Update

--- backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+139643159b0d20de48a3511f9ba2c96856f24cd9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59a908dd8d97215ae574d51e89adb64b2e8f0123")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
