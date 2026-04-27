f6582371c81a49a7cd6fd54c6c1d361281f79ed2
GabyNor05 <160027369+GabyNor05@users.noreply.github.com>
2026-04-28 01:57:57 +0200

    update(obj): commiting most recent AssemblyInfo

--- backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87f663361017b47afd94fb42afa59b4e030daabd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c30f0b77ce9fb31f6bbb57a01361aafffffbc817")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
