4bdd8c468d112417db2d96bd9ade435f25187c03
GabyNor05 <160027369+GabyNor05@users.noreply.github.com>
2026-05-26 11:35:47 +0200

    feat(admin): fixed card width on Overview page

--- frontend/src/pages/admin/Overview.jsx
+++ frontend/src/pages/admin/Overview.jsx
@@ -16,7 +16,7 @@ const DayLabel = () => (
 
 function Overview({ setActiveTab }) {
     return (
-        <div className="flex flex-col gap-8 px-20 w-full font-poppins">
+        <div className="flex flex-col gap-8 px-20 w-full font-poppins pb-8">
             <div className="flex flex-row justify-between items-baseline font-poppins ">
                 <div className="flex flex-col text-left">
                     <h2 className="text-[40px] font-bold mb-1">Overview</h2>
