2ec04432bb5e7273b1bbb18a818b0cfcfe0c055a
KBOlifant <241299@virtualwindow.co.za>
2026-06-09 14:16:34 +0200

    New Judge Functions

--- backend/Models/Judge.cs
+++ backend/Models/Judge.cs
@@ -3,7 +3,7 @@ namespace SheDesign.Models
     public class Judge
     {
         public int Id { get; set; }
-        public int IndustryProfessionalID;
+        public int IndustryProfessionalID { get; set; } 
         public IndustryProfessional? IndustryProfessional { get; set; }
     }
 }
